3323ef3cb8c7398aba09cb2cfc862371fd1665aa
Andrei Belov <a.belov@f5.com>
2025-12-29 17:45:17 -0700

    fedora-cloud-43: fix bootstrap to remove older kernel(s) and remove serial console on arm64 targets

--- fedora-cloud-43/fedora-cloud-43.bootstrap.sh
+++ fedora-cloud-43/fedora-cloud-43.bootstrap.sh
@@ -3,24 +3,35 @@
 set -ex
 umask 022
 
+# update grub config
+awk -i inplace '/console=ttyS/ {print "#" $0; gsub(/console=ttyS[^ "]* */, ""); print; next}
+     /console=ttyAMA/ {print "#" $0; gsub(/console=ttyAMA[^ "]* */, ""); print; next}
+     /^GRUB_TERMINAL_/ {print "#" $0; next}
+     /^GRUB_TIMEOUT=/ {print "#" $0; print "GRUB_TIMEOUT=5"; next} 1' /etc/default/grub
+grub2-mkconfig -o /boot/grub2/grub.cfg
+
+# save current kernel version
+rpm -qa | grep -E -- "kernel-core|kernel-modules-core" >/tmp/kernel_before_update
+
+# update packages and install open-vm-tools
+dnf update -y
+dnf install -y open-vm-tools
+
+# check if new kernel has been installed and remove old one in that case
+rpm -qa | grep -E -- "kernel-core|kernel-modules-core" >/tmp/kernel_after_update
+if ! cmp -s /tmp/kernel_before_update /tmp/kernel_after_update; then
+    rpm -qa | grep -E -- "kernel-core|kernel-modules-core" | grep -- "$(uname -r)" | xargs -r rpm -e
+    find /boot/loader/entries/ -type f -name "*$(uname -r)*" -print -delete
+fi
+
 # stopping services
 systemctl stop systemd-journald.socket systemd-journald-audit.socket systemd-journald-dev-log.socket
 systemctl stop systemd-journald
 
 # initiate cloud-init for next fresh boot
-cloud-init clean --logs --seed
+cloud-init clean --logs --seed --machine-id
 install -m644 /tmp/cloud.cfg /etc/cloud/cloud.cfg
 truncate -s 0 /etc/machine_id
-
-# update grub config
-awk -i inplace '/console=ttyS/ {print "#" $0; gsub(/console=ttyS[^ "]* */, ""); print; next}
-     /^GRUB_TERMINAL_/ {print "#" $0; next}
-     /^GRUB_TIMEOUT=/ {print "#" $0; print "GRUB_TIMEOUT=5"; next} 1' /etc/default/grub
-grub2-mkconfig -o /boot/grub2/grub.cfg
-
-# update packages and install open-vm-tools
-dnf update -y
-dnf install -y open-vm-tools
 
 # clean up logs
 find /var/log/ -type f -print -delete
@@ -50,13 +61,15 @@ sync
 
 # mumbo jumbo to give vmware-vdiskmanager more room for defragmenting and shrinking vmdk disk(s)
 # fedora uses btrfs with zstd compression, hence we need to disable compression for a target file first
-touch /home/vagrant/zeroes
-chattr +m /home/vagrant/zeroes
-if ! dd if=/dev/zero of=/home/vagrant/zeroes bs=64k; then
-    sync
-    rm -f /home/vagrant/zeroes
-    sync
-fi
+for destpath in /boot /home/vagrant; do
+    touch ${destpath}/zeroes
+    chattr +m ${destpath}/zeroes
+    if ! dd if=/dev/zero of=${destpath}/zeroes bs=4k; then
+        sync
+        rm -f ${destpath}/zeroes
+        sync
+    fi
+done
 
 # emit disk usage so it would be in packer log for future references
 df -h
