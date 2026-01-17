f0aa5db8181dd722b4a131a238f067e8b97e6c24
Andrei Belov <defanator@gmail.com>
2026-01-17 16:42:17 -0700

    Amazon Linux 2023: use reduced cloud-init config, more cleanup

--- amazonlinux-2023/amazonlinux-2023.bootstrap.sh
+++ amazonlinux-2023/amazonlinux-2023.bootstrap.sh
@@ -3,23 +3,42 @@
 set -ex
 umask 022
 
+# ensure that initial cloud-init has completed
+cloud-init status --long
+
+# install open-vm-tools
 yum install -y open-vm-tools
 
-systemctl stop amazon-ssm-agent
-systemctl disable amazon-ssm-agent
+# stopping services
 systemctl stop systemd-journald-audit.socket
 systemctl stop systemd-journald-dev-log.socket
 systemctl stop systemd-journald.socket
 systemctl stop systemd-journald
+systemctl stop amazon-ssm-agent
+systemctl disable amazon-ssm-agent
 
-cloud-init clean --logs
+# initiate cloud-init for next fresh boot
+cloud-init clean --logs --seed
+mv /etc/cloud/cloud.cfg /etc/cloud/cloud.cfg.orig
+install -m644 /tmp/cloud.cfg /etc/cloud/cloud.cfg
+rm -f /etc/cloud/cloud.cfg.d/02_amazon-onprem.cfg
 truncate -s 0 /etc/machine_id
 
-rm -f /etc/ssh/ssh_host_*
-rm -rf /var/log/journal/*
+# remove existing interface settings
 rm -f /etc/udev/rules.d/70-persistent-net.rules
 rm -f /etc/sysconfig/network-scripts/ifcfg-eth*
-rm -rf /var/cache/yum
+
+# this one triggers package repository indices fetching on every boot
+rm -f /etc/update-motd.d/70-available-updates
+
+# uninstall optional packages
+rpm -e amazon-ssm-agent awscli-2
+
+# remove cache
+rm -rf /var/cache/dnf
+
+# clean up logs
+rm -rf /var/log/journal/*
 find /var/log/ -type f -print -delete
 
 # install public key for vagrant user
@@ -27,6 +46,9 @@ mkdir -p /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
 curl -fsSL -o /home/vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/hashicorp/vagrant/main/keys/vagrant.pub
 chown -R vagrant:vagrant /home/vagrant/.ssh
+
+# remove existing host keys (these should be regenerated on a first boot)
+rm -f /etc/ssh/ssh_host_*
 
 # synchronize cached writes to persistent storage
 sync
