b867f4d15ca44f4e5c8cd0b8afaeec29b5a436e5
Andrei Belov <defanator@gmail.com>
2026-01-17 13:27:14 -0700

    Reduce diff betseen Amazon Linux 2 and 2023 builders
    While here:
    1. Introduced standalone bootstrap scripts.
    2. Add shutdown_command to gracefully shut down builder VMs.
    3. Add disk space optimization to reduce size of resulting box.

---
+++ amazonlinux-2/amazonlinux-2.bootstrap.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -ex
+umask 022
+
+yum install -y open-vm-tools
+systemctl stop crond
+systemctl stop amazon-ssm-agent
+systemctl disable amazon-ssm-agent
+systemctl stop systemd-journald.socket
+systemctl stop systemd-journald
+
+cloud-init clean --logs
+truncate -s 0 /etc/machine_id
+
+rm -f /etc/ssh/ssh_host_*
+rm -rf /var/log/journal/*
+rm -f /etc/udev/rules.d/70-persistent-net.rules
+rm -f /etc/sysconfig/network-scripts/ifcfg-eth*
+rm -rf /var/cache/yum
+find /var/log/ -type f -print -delete
+
+# install public key for vagrant user
+mkdir -p /home/vagrant/.ssh
+chmod 700 /home/vagrant/.ssh
+curl -fsSL -o /home/vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/hashicorp/vagrant/main/keys/vagrant.pub
+chown -R vagrant:vagrant /home/vagrant/.ssh
+
+# synchronize cached writes to persistent storage
+sync
+
+# mumbo jumbo to give vmware-vdiskmanager more room for defragmenting and shrinking vmdk disk(s)
+if ! dd if=/dev/zero of=/home/vagrant/zeroes bs=4k; then
+    sync
+    rm -f /home/vagrant/zeroes
+    sync
+fi
+
+# emit disk usage so it would be in packer log for future references
+df -h
